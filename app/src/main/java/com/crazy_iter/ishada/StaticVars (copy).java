734a450d6a36991b6258871a8666e68e9dc59ff9
Ahmad <alsaleh.a.ahmad@gmail.com>
2018-11-15 20:45:17 +0200

    test2

---
+++ app/src/main/java/com/crazy_iter/ishada/StaticVars (copy).java
@@ -0,0 +1,23 @@
+package com.crazy_iter.ishada;
+
+public interface StaticVars {
+
+    String name = "ishada";
+
+    // region APIs
+
+    String URL_BASE = "https://ishada.herokuapp.com/";
+    String TASKS_BASE = URL_BASE + "tasks/";
+    String ASSIGN_TASK = TASKS_BASE + "assignTask/";
+    String CHANGE_STATUS = TASKS_BASE + "changeStatus/";
+    String DELETE_TASK = TASKS_BASE + "deleteTask/";
+    String NEW_TASK = TASKS_BASE + "newTask/";
+
+    String USERS_BASE = URL_BASE + "users/";
+    String SIGN_IN = USERS_BASE + "signin/";
+    String SIGN_UP = USERS_BASE + "signup/";
+    String USERS_BY_IDs = USERS_BASE + "getUsersByIDs/";
+
+    // endregion
+
+}
